4b51c37a1ca94715133af5d479ddb2ef34eb3fd1
ephrem <ephrem@ephrem-Latitude-E6540.localdomain>
2026-05-04 17:29:08 -0700

    init

---
+++ ats-parkaccess/src/main/java/com/ats/parkaccess/AtsParkaccessApplication.java
@@ -0,0 +1,13 @@
+package com.ats.parkaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsParkaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsParkaccessApplication.class, args);
+	}
+
+}
---
+++ ats-parkaccess/src/test/java/com/ats/parkaccess/AtsParkaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.parkaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsParkaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
